d235655ba8b385e29ea6c5fb22fa8920c893646f
VuThiBichThu <thuvu.24hdev@gmail.com>
2021-10-26 14:55:40 +0700

    Props, useState và useEffect

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,88 @@
+import {
+  AppBar,
+  Box,
+  Button,
+  FormControl,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
+import React, { useState, ChangeEvent, useEffect } from "react";
+import WelcomeMessage from "./WelcomeMessage";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    positionSelect: {
+      color: "white",
+      borderBottom: "1px solid white",
+    },
+  })
+);
+const Navbar = () => {
+  // styles
+  const classes = useStyles();
+
+  // state
+  const [position, setPosition] = useState<string>("Full-stack Developer");
+
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+
+  // useEffect
+  useEffect(() => {
+    const timer = setInterval(() => setTime(new Date(Date.now())), 1000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const onPositionChange = (
+    event: ChangeEvent<{
+      value: unknown;
+    }>
+  ) => setPosition(event.target.value as string);
+  return (
+    <AppBar position="static" color="primary">
+      <Toolbar>
+        <Box
+          display="flex"
+          justifyContent="space-between"
+          alignItems="center"
+          width={1}
+          py={2}
+        >
+          <Typography variant="h6">My Movies</Typography>
+          <Box textAlign="center">
+            <WelcomeMessage position={position} />
+            <Box mt={1}>
+              <FormControl>
+                <Select
+                  value={position}
+                  onChange={onPositionChange}
+                  className={classes.positionSelect}
+                >
+                  <MenuItem value="Full-stack Developer">
+                    Full-stack Developer
+                  </MenuItem>
+                  <MenuItem value="Front-end Developer">
+                    Front-end Developer
+                  </MenuItem>
+                  <MenuItem value="Back-end Developer">
+                    Back-end Developer
+                  </MenuItem>
+                </Select>
+              </FormControl>
+            </Box>
+          </Box>
+          <Box textAlign="center">
+            <Box my={1}>
+              <Typography variant="h6">{time.toUTCString()}</Typography>
+            </Box>
+            <Button variant="contained">Login</Button>
+          </Box>
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
